8f588c126f7d99f249f8cadc50c11ff70fad724b
Gustavo62 <gust904@gmail.com>
2020-10-08 12:28:55 -0300

    corrigindo contatos

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log
 
 job_type :backup, "backup perform --trigger SINJUR"
 
-every 1.minute do
-    backup ""
+every :day, at: '13:00 pm' do
+    command "backup perform --trigger SINJUR"
 end
